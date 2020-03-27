7630d6692634637fbafa944c5aa263159fa47611
cvgellhorn <gellborn@gmail.com>
2020-03-27 17:21:02 +1100

    add search

---
+++ assets/js/search.js
@@ -0,0 +1,46 @@
+---
+---
+var inputWrapper = document.getElementById('search');
+var searchInput = document.getElementById('search-input');
+var postContainer = document.getElementById('post-container');
+var resultContainer = document.getElementById('post-result-container');
+var postPager = document.getElementById('post-pager');
+
+var sjs = SimpleJekyllSearch({
+    success: function() {},
+    searchInput: document.createElement('INPUT'),
+    resultsContainer: resultContainer,
+    searchResultTemplate: '{html}',
+    noResultsText: '<div class="post-search-no-result"><h3>{{ site.translations.no_result }}</h3></div>',
+    json: '/data.json',
+    limit: 12
+});
+
+var value = '';
+searchInput.addEventListener('keyup', function(event) {
+    // ESC
+    if (event.keyCode == 27) {
+        searchInput.value = '';
+        searchInput.blur();
+    }
+    
+    if (value === event.target.value) {
+        return false;
+    } else {
+        value = event.target.value;
+    }
+    
+    var cond = !!value;
+    postPager.classList.toggle('hidden', cond);
+    postContainer.classList.toggle('hidden', cond);
+    resultContainer.classList.toggle('hidden', !cond);
+
+    sjs.search(value);
+    return false;
+});
+searchInput.addEventListener('focus', function() {
+    inputWrapper.classList.add('focused');
+});
+searchInput.addEventListener('blur', function() {
+    inputWrapper.classList.remove('focused');
+});
